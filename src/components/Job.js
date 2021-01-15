918f27665a5850dd69b9f6fd1a91433fcb503d99
Mahin Sagotra <mahinsagotra18@outlook.com>
2021-01-15 16:31:19 +0530

    Basic jobs cards done

--- src/components/Job.js
+++ src/components/Job.js
@@ -1,9 +1,22 @@
-import React from 'react'
+import React from "react";
+import { Card } from "react-bootstrap";
 
 export default function Job({ job }) {
-    return (
-        <div>
-            {job.title}
+  return (
+    <Card>
+      <Card.Body>
+        <div className="d-flex jsutify-content-between">
+          <div>
+            <Card.Title>
+              {job.title} -
+              <span className="text-muted font-weight-light">
+                {job.company}
+              </span>
+            </Card.Title>
+          </div>
         </div>
-    )
+      </Card.Body>
+      {job.title}
+    </Card>
+  );
 }
