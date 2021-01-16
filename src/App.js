4987d6eb35185ddc0141afde7624a8350c5d0c8a
Mahin Sagotra <mahinsagotra18@outlook.com>
2021-01-16 13:17:13 +0530

    Added Form and Project Done

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import React, { useState } from 'react'
-import useFetchJobs from './components/useFetchJobs'
-import { Container } from 'react-bootstrap'
-import Job from './components/Job'
-import JobsPagination from './components/JobsPagination'
+import React, { useState } from "react";
+import useFetchJobs from "./components/useFetchJobs";
+import { Container } from "react-bootstrap";
+import Job from "./components/Job";
+import JobsPagination from "./components/JobsPagination";
+import SearchFrom from "./components/SearchFrom";
 
 function App() {
-    const [params, setParams] = useState({})
-    const [page, setPage] = useState(1)
-    const { jobs, loading, error, hasNextPage } = useFetchJobs(params, page)
-    return (
-        <Container className='my-4'>
-            <h1 className='mb-4'>Github Jobs!</h1>
-            <JobsPagination page={page} setPage={setPage} hasNextPage={hasNextPage}/>
-            {loading && <h1> Loading... </h1>}
-            {error && <h1> Error.Try Refreshing. </h1>}
-            {jobs.map(job => {
-                return <Job key={job.id} job={job} />
-            })}
-            <JobsPagination page={page} setPage={setPage} hasNextPage={hasNextPage}/>
-        </Container>
-    );
+  const [params, setParams] = useState({});
+  const [page, setPage] = useState(1);
+  const { jobs, loading, error, hasNextPage } = useFetchJobs(params, page);
+
+  function handleParamChange(e) {
+    const param = e.target.name;
+    const value = e.target.value;
+    setPage(1);
+    setParams((prevParams) => {
+      return { ...prevParams, [param]: value };
+    });
+  }
+
+  return (
+    <Container className="my-4">
+      <h1 className="mb-4">Github Jobs!</h1>
+      <SearchFrom params={params} onParamChange={handleParamChange} />
+      <JobsPagination page={page} setPage={setPage} hasNextPage={hasNextPage} />
+      {loading && <h1> Loading... </h1>}
+      {error && <h1> Error.Try Refreshing. </h1>}
+      {jobs.map((job) => {
+        return <Job key={job.id} job={job} />;
+      })}
+      <JobsPagination page={page} setPage={setPage} hasNextPage={hasNextPage} />
+    </Container>
+  );
 }
 export default App;
